07c4227f31e591913b9f6cc7a91289264c0538e5
Ojingwa Daniel <ojingwadanny@gmail.com>
2026-06-11 16:59:21 +0100

    added livewire package for web page dynamics

---
+++ app/Models/Option.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Option extends Model
+{
+    //
+    public function polls(){
+        return $this->belongsTo(Poll::class);
+    }
+    public function votes(){
+        return $this->hasMany(Vote::class);
+    }
+}
---
+++ app/Models/Poll.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Poll extends Model
+{
+    //
+    public function options(){
+        return $this->hasMany(Option::class);
+    }
+    // public function votes(){
+    //     return $this->hasMany(Vote::class);
+    // }
+}
