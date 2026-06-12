395f3e4b9bec6c4bddd7e191b624a18fe22a4ab7
Ojingwa Daniel <ojingwadanny@gmail.com>
2026-06-12 20:12:12 +0100

    added livewire component and livewire volt

--- resources/views/components/⚡create-poll.blade.php
+++ resources/views/components/⚡create-poll.blade.php
@@ -1,13 +1,19 @@
 <?php
 
-use Livewire\Component;
+
+use Livewire\Volt\Component;
 
 new class extends Component
 {
     //
+    public $title;
+    
 };
 ?>
 
 <div>
-    {{-- Be present above all else. - Naval Ravikant --}}
+    {{-- Very little is needed to make a happy life. - Marcus Aurelius --}}
+    <label for="">Poll title</label>
+    <input type="text" wire:model.live="title"/>
+    Current Title  {{$title}}
 </div>
